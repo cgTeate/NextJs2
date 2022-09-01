4295138a01acc2946164bf1e80050f66d9aa0b66
Calvin Teater <laughingglint3@gmail.com>
2022-09-01 10:59:56 -0500

    style buttons container

--- components/Post.js
+++ components/Post.js
@@ -17,8 +17,8 @@ export default function Post({img,userImg,caption,username,id}) {
       <img className="object-cover w-full" src={img} alt="" />     
 
       {/* Post Buttons */}
-      <div className="">
-          <div className="">
+      <div className="flex justify-between px-4 pt-4">
+          <div className="flex space-x-4">
               <HeartIcon className="btn"/>
               <ChatBubbleOvalLeftEllipsisIcon className="btn"/>
           </div>
