66d613c1656818c4b9501d115442f46c57aa53c8
Calvin Teater <laughingglint3@gmail.com>
2022-08-31 19:48:14 -0500

    implement sticky header

--- components/Header.js
+++ components/Header.js
@@ -7,6 +7,7 @@ import React from "react";
 
 export default function Header() {
   return (
+    <div className="shadow-sm border-b sticky top-0 bg-white z-30">
         <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
               {/* Left */}
             <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
@@ -50,5 +51,7 @@ export default function Header() {
         />
       </div>
     </div>
+    </div>
+        
   )
 }
