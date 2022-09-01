001612f4a7e3464ddddd0baf5693c07c08f7401d
Calvin Teater <laughingglint3@gmail.com>
2022-08-31 19:08:33 -0500

    add instagram logo to the left side of the header components

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,34 @@
+import Image from "next/image";
 
 
 export default function Header() {
   return (
     <div>
-        <h1>Header</h1>
+        {/* Left */}
+
+        <div className="flex items-center justify-between max-w-6xl">
+            <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
+                <Image
+                src="http://www.jennexplores.com/wp-content/uploads/2015/09/Instagram_logo_black.png"
+                layout="fill"
+          className="object-contain"
+                />
+            </div>
+            <div className="cursor-pointer h-24 w-10 relative  lg:hidden">
+                <Image
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/800px-Instagram_logo_2016.svg.png"
+                layout="fill"
+          className="object-contain"
+                />
+            </div>
+
+            <h1>Right Side</h1>
+        </div>
+        
+        {/* Middle */}
+
+
+        {/* Right */}
     </div>
   )
 }
