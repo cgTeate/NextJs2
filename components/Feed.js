7d144ab2b6715e03cafd881dfb76196d939ccd15
Calvin Teater <laughingglint3@gmail.com>
2022-08-31 19:56:48 -0500

    create feed component

---
+++ components/Feed.js
@@ -0,0 +1,19 @@
+
+
+export default function Feed() {
+  return (
+    <main>
+    <section>
+     {/* Stories */}
+    
+     {/* Posts */}
+   </section>
+
+   <section>
+     {/* Mini Profile */}
+
+     {/* Suggestions */}
+   </section>
+</main>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Feed from '../components/Feed'
 import Header from '../components/Header'
 
 export default function Home() {
@@ -15,7 +16,7 @@ export default function Home() {
      <Header/>
       {/* Feed */}
 
-
+      <Feed/>
       {/* Modal */}
     </div>
   )
