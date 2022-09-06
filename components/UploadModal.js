a1726849707a1c5edce79445837969b98c190d2c
Calvin Teater <laughingglint3@gmail.com>
2022-09-06 08:57:43 -0500

    Show image in modal container

--- components/UploadModal.js
+++ components/UploadModal.js
@@ -2,10 +2,22 @@ import { useRecoilState } from "recoil";
 import { modalState } from "../atom/modalAtom";
 import Modal from "react-modal";
 import { CameraIcon } from "@heroicons/react/24/outline";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 export default function UploadModal() {
   const [open, setOpen] = useRecoilState(modalState);
+  const [selectedFile, setSelectedFile] = useState(null)
+  function addImageToPost(event){
+    const reader = new FileReader()
+    if(event.target.files[0]){
+      reader.readAsDataURL(event.target.files[0])
+    }
+
+    reader.onload = (readerEvent) => {
+      setSelectedFile(readerEvent.target.result)
+    }
+    
+  }
   const filePickerRef = useRef(null)
   return (
     <div>
@@ -16,8 +28,10 @@ export default function UploadModal() {
           onRequestClose={() => setOpen(false)}
         >
           <div className="flex flex-col justify-center items-center h-[100%]">
-            <CameraIcon onClick={()=>filePickerRef.current.click()} className="cursor-pointer h-14 bg-red-200 p-2 rounded-full border-2 text-red-500"/>
-            <input type="file" hidden ref={filePickerRef}/>
+            {selectedFile ? (
+              <img src={selectedFile} alt=""/>
+            ) : (<CameraIcon onClick={()=>filePickerRef.current.click()} className="cursor-pointer h-14 bg-red-200 p-2 rounded-full border-2 text-red-500"/>)}
+            <input type="file" hidden ref={filePickerRef} onChange={addImageToPost}/>
             <input type="text" maxLength={150} placeholder="Please enter your caption..." className="m-4 border-none text-center w-full focus:ring-0"/>
             <button disabled className="w-full bg-red-600 text-white p-2 shadow-md hover:brightness-125 disabled:bg-gray-200 disabled:cursor-pointer-not-allowed disbaled:hover:brightness-100">Upload Post</button>
           </div>
